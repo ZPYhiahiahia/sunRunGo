3df35ffb2fd287a5f8b35387d04eb5f0c409a711
diifferent <970936604@qq.com>
2018-04-14 09:17:51 +0800

    添加定时启动

--- wait_for_0600.py
+++ wait_for_0600.py
@@ -6,8 +6,9 @@ def wait_for_0600():
 
     day = now_time.day + 1
     hour = 6
-    while(now_time.day != day and now_time.hour != 6):
-            time.sleep(360)
-            now_time = datetime.datetime.now()
-            print("等待开始...")
+    while(now_time.day != day or now_time.hour != 6):
+        print("等待开始...")
+        time.sleep(2)
+        now_time = datetime.datetime.now()
+
 
